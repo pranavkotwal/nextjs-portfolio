62304ee5647deaf442b07f1a3d04f9c205bd4418
pranav kotwal <pkotwal24@gmail.com>
2024-03-29 09:12:18 +0530

    add ui files

---
+++ app/ui/fonts.js
@@ -0,0 +1,13 @@
+import { Inter,DM_Serif_Display } from "next/font/google";
+
+export const inter = Inter({
+    subsets:['latin'],
+    weight:['600','300'],
+    display:'swap'
+})
+
+export const dm_serif_display = DM_Serif_Display({
+    subsets:['latin'],
+    display:['swap'],
+    weight:['400','700']
+})
